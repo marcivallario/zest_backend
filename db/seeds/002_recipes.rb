430b0c77237d252a057c934fd4b73b49931f0f8e
Logan <loganfranklinsmith@hotmail.com>
2022-01-26 18:40:46 -0500

    rating added to recipes and random rating to seed

--- db/seeds/002_recipes.rb
+++ db/seeds/002_recipes.rb
@@ -1,13 +1,13 @@
 
 #Recipes
 puts  "🌱 Seeding recipes..."
-$r1 = Recipe.create(name: "Steak Tacos ", picture_url: "", cuisine_id: $c1.id)
-$r2 = Recipe.create(name: "Horchata", picture_url: "", cuisine_id: $c1.id)
-$r3 = Recipe.create(name: "Churros", picture_url: "", cuisine_id: $c1.id)
+$r1 = Recipe.create(name: "Steak Tacos ", picture_url: "", rating: rand(5), cuisine_id: $c1.id)
+$r2 = Recipe.create(name: "Horchata", picture_url: "", rating: rand(5),  cuisine_id: $c1.id)
+$r3 = Recipe.create(name: "Churros", picture_url: "", rating: rand(5), cuisine_id: $c1.id)
 
-$r4 = Recipe.create(name: "Edamame", picture_url: "", cuisine_id: $c2.id)
-$r5 = Recipe.create(name: "Agedashi Tofu", picture_url: "", cuisine_id: $c2.id)
-$r6 = Recipe.create(name: "Cucumber Salad", picture_url: "", cuisine_id: $c2.id)
+$r4 = Recipe.create(name: "Edamame", picture_url: "", rating: rand(5), cuisine_id: $c2.id)
+$r5 = Recipe.create(name: "Agedashi Tofu", picture_url: "", rating: rand(5), cuisine_id: $c2.id)
+$r6 = Recipe.create(name: "Cucumber Salad", picture_url: "", rating: rand(5), cuisine_id: $c2.id)
 
 #$r7 = Recipe.create(name: "", picture_url: "", cuisine_id: $c3.id)
 #$r8 = Recipe.create(name: "", picture_url: "", cuisine_id: $c3.id)
