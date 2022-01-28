6e53a74c11cfaced288598c74532c32f73f9ea0d
Marci Vallario <marcivallario@gmail.com>
2022-01-27 19:16:46 -0500

    Deleted duplicate seed data in ingredient list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
     
-    get "/recipes" do
-      Recipe.all.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
-    end
-  
-    patch "/recipes/:id" do
-      recipe = Recipe.find(params[:id])
-      recipe.update(rating: params[:rating])
-      recipe.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
-    end
-  
-    delete "/recipes/:id" do
-      recipe = Recipe.find(params[:id])
-      recipe.destroy
-      recipe.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
-    end
+  get "/recipes" do
+    Recipe.all.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
   end
 
+  patch "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.update(rating: params[:rating])
+    recipe.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
+  end
+
+  delete "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    recipe.to_json(include: [:cuisine, :directions, :ingredients, :ingredient_lists])
+  end
 end
--- db/seeds/004_ingredient_lists.rb
+++ db/seeds/004_ingredient_lists.rb
@@ -20,7 +20,6 @@ IngredientList.create(quantity:"1 ", recipe_id: $r2.id , ingredient_id: $i13.id 
 IngredientList.create(quantity:"1 1/2 teaspoons ", recipe_id: $r2.id , ingredient_id: $i14.id )
 IngredientList.create(quantity:"4 cups hot", recipe_id: $r2.id , ingredient_id: $i15.id )
 IngredientList.create(quantity:"1 cup ", recipe_id: $r2.id , ingredient_id: $i16.id )
-IngredientList.create(quantity:"1 cup cold ", recipe_id: $r2.id , ingredient_id: $i15.id )
 IngredientList.create(quantity:"(optional) ", recipe_id: $r2.id , ingredient_id: $i17.id )
 IngredientList.create(quantity:"(optional) ", recipe_id: $r2.id , ingredient_id: $i18.id )
 
@@ -51,7 +50,6 @@ IngredientList.create(quantity:"1 tablespoon ", recipe_id: $r5.id , ingredient_i
 IngredientList.create(quantity:"1 tablespoon ", recipe_id: $r5.id , ingredient_id: $i15.id )
 IngredientList.create(quantity:"1 tablespoon freshly grated ", recipe_id: $r5.id , ingredient_id: $i34.id )
 IngredientList.create(quantity:"5 tablespoon ", recipe_id: $r5.id , ingredient_id: $i33.id )
-IngredientList.create(quantity:"(optional) 1/3 cup ", recipe_id: $r5.id , ingredient_id: $i34.id )
 
 IngredientList.create(quantity:"2 cups thinly sliced ", recipe_id: $r6.id , ingredient_id: $i36.id )
 IngredientList.create(quantity:"1 teaspoon ", recipe_id: $r6.id , ingredient_id: $i21.id )
